20cde8b81d178a1dbc8eb87189b72f4471ad2fb0
pinkWar123 <131501454+pinkWar123@users.noreply.github.com>
2024-08-22 22:56:52 +0700

    Handle refresh token

--- client/MiniIeltsClone/src/App.tsx
+++ client/MiniIeltsClone/src/App.tsx
@@ -1,37 +1,88 @@
 import SearchPage from "./pages/SearchPage/SearchPage";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import AuthLayout from "./layouts/AuthLayout/AuthLayout";
 import RegisterPage from "./pages/AuthPage/RegisterPage";
 import LoginPage from "./pages/AuthPage/LoginPage";
 import useUser from "./hooks/useUser";
 import { useEffect } from "react";
-import { getUserByToken } from "./services/authentication";
+import { getUserByToken, refreshTokens } from "./services/authentication";
 import CreateTestPage from "./pages/CreateTestPage/CreateTestPage";
 import { TestProvider } from "./contexts/TestContext";
 import useMessage from "antd/es/message/useMessage";
 import DoTestPage from "./pages/DoTestPage/DoTestPage";
 import { AnswersProvider } from "./contexts/AnswertContext";
 import TestResultPage from "./pages/TestResultPage/TestResultPage";
+import { App as AntdApp } from "antd";
+import axiosInstance from "./services/axiosConfig";
 function App() {
   const { setUser } = useUser();
-  const [_, contextHolder] = useMessage();
+  const contextHolder = useMessage();
+  const { modal } = AntdApp.useApp();
+  const navigate = useNavigate();
   useEffect(() => {
-    const getUser = async () => {
-      const token = localStorage.getItem("token");
-      if (!token) return;
-      const res = await getUserByToken(token);
-      console.log("Get user by token:", res.data);
-      if (res?.status === 200) {
-        setUser(res.data);
+    const requestInterceptor = axiosInstance.interceptors.request.use(
+      (config) => {
+        // You can modify the config before the request is sent
+        // For example, attach an authorization token
+        const token = localStorage.getItem("token");
+        if (token) {
+          config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+      },
+      (error) => {
+        // Do something with request error
+        console.log(error);
+        return Promise.reject(error);
       }
+    );
+
+    const responseInterceptor = axiosInstance.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      async (error) => {
+        console.log(error);
+        const originalRequest = error.config;
+        console.log(originalRequest);
+        if (error.response.status === 401) {
+          try {
+            const user = await refreshTokens();
+            console.log(user);
+            if (user) {
+              localStorage.setItem("access_token", user.token);
+              setUser(user);
+              modal.success({ content: "refresh success" });
+              return Promise.resolve();
+            }
+          } catch (error) {
+            modal.warning({
+              content:
+                "Phiên đăng nhập của bạn đã hết, vui lòng đăng nhập lại!",
+              onOk: () => {
+                navigate("/auth/login", { replace: true });
+              },
+            });
+          }
+
+          return Promise.reject(error);
+        }
+        return Promise.reject(error);
+      }
+    );
+
+    // Return cleanup function to remove interceptors if necessary
+    return () => {
+      axiosInstance.interceptors.request.eject(requestInterceptor);
+      axiosInstance.interceptors.response.eject(responseInterceptor);
     };
-    getUser();
-  }, [setUser]);
+  }, [navigate]);
+
   return (
     <>
-      {contextHolder}
-      <BrowserRouter>
+      {contextHolder[1]}
+      <>
         <Routes>
           <Route path="/" element={<SearchPage />} />
           <Route path="auth" element={<AuthLayout />}>
@@ -63,7 +114,7 @@ function App() {
             }
           />
         </Routes>
-      </BrowserRouter>
+      </>
     </>
   );
 }
